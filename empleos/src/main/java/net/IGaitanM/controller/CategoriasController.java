5a9630f983a72c4da373417e6bf58fe5bad7d8b5
IGaitanM <ivanovef@gmail.com>
2021-06-29 13:24:20 +0200

    renderizo listCategorias, url del boton crear, métodos mostrarIndez y guardar con validadcion de errores DataBinding, nuevo menu

--- empleos/src/main/java/net/IGaitanM/controller/CategoriasController.java
+++ empleos/src/main/java/net/IGaitanM/controller/CategoriasController.java
@@ -1,38 +1,57 @@
 package net.IGaitanM.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import net.IGaitanM.model.Categoria;
+import net.IGaitanM.service.ICategoriasService;
 
 @Controller
 @RequestMapping(value="/categorias")
 public class CategoriasController {
 	
+	@Autowired
+   	private ICategoriasService serviceCategorias;
+	
 	// @GetMapping("/index")
 	@RequestMapping(value="/index", method=RequestMethod.GET)
 	public String mostrarIndex(Model model) {
-		return "categorias/listCategorias";
+		List<Categoria> lista = serviceCategorias.buscarTodas();
+    	model.addAttribute("categorias", lista);
+		return "categorias/listCategorias";		
 	}
+	
 	// @GetMapping("/create")
 	/**
 	 * Método para crear categorías a través del formulario formCategoria.html.
 	 */
 	@RequestMapping(value="/create", method=RequestMethod.GET)
-	public String crear() {
+	public String crear(Categoria categoria) {
 		return "categorias/formCategoria";
 	}
+	
 	// @PostMapping("/save")
 	/**
 	 * Método que guarda los valores del formulario formCategoria.html en sus variables correspondientes.
 	 */
 	@RequestMapping(value="/save", method=RequestMethod.POST)
-	public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion") String descripcion) {
-		System.out.println("Categoria:" + nombre);
-		System.out.println("Descripcion: " + descripcion);
+	public String guardar(Categoria categoria, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()){		
+			System.out.println("Existieron errores");
+			return "categorias/formCategoria";
+		}	
 		
-		return "categorias/listCategorias";
-	}
-	
+		// Guadamos el objeto categoria en la bd
+				serviceCategorias.guardar(categoria);
+				attributes.addFlashAttribute("msg", "Los datos de la categoría fueron guardados!");		
+				return "redirect:/categorias/index";
+			}
 }
+	
+
---
+++ empleos/src/main/java/net/IGaitanM/service/ICategoriasService.java
@@ -0,0 +1,11 @@
+package net.IGaitanM.service;
+
+import java.util.List;
+
+import net.IGaitanM.model.Categoria;
+
+public interface ICategoriasService {
+	void guardar(Categoria categoria);
+	List<Categoria> buscarTodas();
+	Categoria buscarPorId(Integer idCategoria);	
+}
