d9666d804e5c4957a5509e86100ad7eb8119d529
IGaitanM <ivanovef@gmail.com>
2021-06-30 13:31:31 +0200

    se crea la relaccion entre la clase vacnte y la clase categoria. DataBinding entre el imput select y la clase categoria

--- empleos/src/main/java/net/IGaitanM/model/Vacante.java
+++ empleos/src/main/java/net/IGaitanM/model/Vacante.java
@@ -17,6 +17,7 @@ public class Vacante {
 	private String imagen="no-image.png";
 	private String estatus;
 	private String detalles;
+	private Categoria categoria;
 	
 	
 
@@ -95,12 +96,23 @@ public class Vacante {
 		this.imagen = imagen;
 	}
 
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
 	@Override
 	public String toString() {
 		return "Vacante [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", fecha=" + fecha
 				+ ", salario=" + salario + ", destacado=" + destacado + ", imagen=" + imagen + ", estatus=" + estatus
-				+ ", detalles=" + detalles + "]";
+				+ ", detalles=" + detalles + ", categoria=" + categoria + "]";
 	}
+
+	
+	
 
 	
 
